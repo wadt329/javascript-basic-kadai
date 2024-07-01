a055838e9222bfe179e4fe3129962bba50e7314b
wadt329 <ttshk1582@gmail.com>
2024-07-01 21:18:46 +0900

    5章課題をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+//体重の変数を宣言・代入
+let bodyWeight = 68
+
+//身長の変数を宣言・代入
+let bodyHeight = 1.7
+
+//BMIの変数を宣言・代入
+let bmi = (bodyWeight / (bodyHeight * bodyHeight))
+
+//BMIの値をコンソールに出力
+console.log(bmi)
+
+
+
